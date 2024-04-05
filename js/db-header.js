5fa074297bf2066553ccce138161b396b204a9b2
untypequicode <untypequicode@gmail.com>
2024-04-05 22:28:32 +0200

    Project initialization

---
+++ js/db-header.js
@@ -0,0 +1,31 @@
+const db_header = [
+    {
+        text: "Accueil",
+        url: "https://untypequicode.github.io/script.js",
+    },
+    {
+        text: "Documentation",
+        url: "documentation",
+    },
+    {
+        text: "Exemples",
+        url: "exemples",
+    },
+    {
+        text: "A propos",
+        url: "a-propos",
+    },
+    {
+        text: "Style.css",
+        url: "https://untypequicode.github.io/style.css",
+    },
+];
+
+const logo = {
+    type: "text",
+    // type: "image",
+    content: "script.js",
+    // content: "images/favicon.svg",
+    link: "https://untypequicode.github.io/script.js",
+    index: "Accueil",
+}
