315e840c914b39faddadc352ef335d467329bf6d
untypequicode <untypequicode@gmail.com>
2024-05-26 02:30:43 +0200

    Add the link to the legal notices in the site's footer.

--- js/db-footer.js
+++ js/db-footer.js
@@ -1,6 +1,6 @@
 const db_footer = {
     left: {
         content: 'Copyright \u00A9 2024 Script.js | Tous droits réservés.',
-        link: 'https://untypequicode.github.io/script.js/'
+        link: 'mentions-legales'
     }
 }
